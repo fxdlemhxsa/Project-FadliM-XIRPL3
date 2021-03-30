472887956e0d19937448bc7db7eb5899847697d9
fxdlemhxsa <fadli.mahesa4578@gmail.com>
2021-03-30 20:39:17 +0700

    Implementasi Kelas Ramen, Kuah, Toping, dan Minuman

--- ProjectKasir/src/classes/Minuman.java
+++ ProjectKasir/src/classes/Minuman.java
@@ -11,6 +11,12 @@ package classes;
  */
 public class Minuman extends Menu {
     
-    public Minuman (String nama_minuman, double harga) { }
+    public Minuman (String nama_minuman, double harga) {
+
+        setNama_menu(nama_minuman);
+        setHarga (harga);
+        setKategori ("Minuman");
+
+     }
 
 }
